c4691782f2fa3cb5949229bea8a56ca4c3d1320e
Shuaib Ali <168617511+Shuaib-ali@users.noreply.github.com>
2026-01-29 18:39:15 +0000

    dataTypes and variables

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,16 @@
+const accountId = 12345
+let accountEmail = "shuaib34@gamil.com"
+var accountPassword = "1234567"
+accountCity = "Delhi"
+let accountState;
+
+//accountId = 2   //not allowed
+//console.log(accountId) 
+/*
+Prefer not to use var 
+because of issue in block scope and functional scope
+*/
+accountEmail = "shuaibtrk34@gamil.com"
+accountPassword = "12345646"
+accountCity = "noida"
+console.table([accountId,accountEmail,accountPassword,accountCity,accountState])
