2682c9bec58a9be690ed4330005e6310c80c53a5
Shuaib Ali <168617511+Shuaib-ali@users.noreply.github.com>
2026-01-31 13:54:07 +0000

    Date in JS

---
+++ 01_basics/07_datesinJs.js
@@ -0,0 +1,25 @@
+// let myDate = new Date() 
+// console.log(myDate.toString());
+// console.log(myDate.toDateString());
+// console.log(myDate.toLocaleString());
+
+// console.log(typeof myDate);
+
+// let myCreatedDate = new Date (2026,0,31)
+// let myCreatedDate = new Date(2026,0,31,11,50)
+let myCreatedDate = new Date("2026-01-31")
+// console.log(myCreatedDate.toLocaleString());
+
+let myTimeStamp = Date.now()
+
+// console.log(myTimeStamp);
+// console.log(myCreatedDate.getTime());
+
+let newDate = new Date()
+// console.log(newDate);
+// console.log(newDate.getMonth());
+// console.log(newDate.getDay());
+
+// newDate.toLocaleString('default',{
+//     weekday:"long",
+// })
